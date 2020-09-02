ead64fba4088e95c5dfc017e9cae08dad45f8064
21cadams <21cadams@athenian.org>
2020-09-02 22:21:09 +0000

    Made the chatbot homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         // Write some code here!
-        System.out.println("Hello, world!");
-    }
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scan.next();
+        System.out.println("Hello " + name +"! It is nice to meet you");
+        
+        System.out.print("What is your favorite sport? ");
+        String sport = scan.next();
+        System.out.println("Oh, I like " +sport+ " too.");
+
+        System.out.print("What is your favorite movie? ");
+        String movie = scan.next();
+        System.out.println("I think that " + movie + " is a great movie too");
+
+        System.out.print("What is your favorite food? ");
+        String food = scan.next();
+        System.out.println(food +" is really good, but I like pizza the most!");
+
+        System.out.print("What is your favorite song? ");
+        String song = scan.next();
+        System.out.println("I have never heard " + song + ", but I will have to listen to it.");
+
+    }   
 }
